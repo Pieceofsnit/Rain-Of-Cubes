27bbda8871fe2fd17daf31e2d18549e0a82dfd3f
Pieceofsnit <102411114+Pieceofsnit@users.noreply.github.com>
2024-06-25 23:18:46 +0500

    correction project

--- Scripts/Cube.cs
+++ Scripts/Cube.cs
@@ -1,48 +1,66 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Events;
 
 [RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(Rigidbody))]
 
 public class Cube : MonoBehaviour
 {
     private MeshRenderer _renedererColor;
     private Coroutine _coroutine;
+    private Rigidbody _rigidbody;
     private float _minLifeTime = 2f;
     private float _maxLifeTime = 5f;
     private bool _isTouched = false;
 
+    public event UnityAction<Cube> OnReleased;
+
     private void Awake()
     {
         _renedererColor = GetComponent<MeshRenderer>();
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void OnEnable()
+    {
+        ResetParameters();
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (_isTouched == false)
         {
-            if (collision.gameObject.TryGetComponent(out Platform wall))
+            if (collision.collider.TryGetComponent(out Platform platform))
             {
                 _renedererColor.material.color = Random.ColorHSV();
-                _coroutine = StartCoroutine(WaitForDestruction());
+                _coroutine = StartCoroutine(WaitForDeactivation());
                 _isTouched = true;
             }
         }
     }
 
-    private IEnumerator WaitForDestruction()
+    private IEnumerator WaitForDeactivation()
     {
         float currentLifeTime = Random.Range(_minLifeTime, _maxLifeTime);
         yield return new WaitForSeconds(currentLifeTime);
-        Destroy();
+        Deactivate();
     }
 
-    private void Destroy()
+    private void ResetParameters()
+    {
+        _rigidbody.velocity = Vector3.zero;
+        _renedererColor.material.color = Color.white;
+        _isTouched = false;
+    }
+
+    private void Deactivate()
     {
         if(_coroutine != null)
         {
             StopCoroutine(_coroutine);
         }
 
-        Destroy(gameObject);
+        OnReleased?.Invoke(this);
     }
 }
--- Scripts/Spawner.cs
+++ Scripts/Spawner.cs
@@ -1,49 +1,55 @@
 using UnityEngine.Pool;
 using UnityEngine;
-using System.Collections.Generic;
 
 public class Spawner : MonoBehaviour
 {
-    [SerializeField] private GameObject _prefabCube;
+    [SerializeField] private Cube _prefabCube;
     [SerializeField] private Transform[] _startPositions;
     [SerializeField] private float _repeatRate = 1f;
     [SerializeField] private int _poolCapacity = 5;
     [SerializeField] private int _poolMaxSize = 5;
 
-    private ObjectPool<GameObject> _pool;
+
+    private ObjectPool<Cube> _pool;
 
     private void Awake()
     {
-        _pool = new ObjectPool<GameObject>(
-        createFunc: () => Instantiate(_prefabCube),
-        actionOnGet: (obj) => ActionOnGet(obj),
-        actionOnRelease: (obj) => obj.SetActive(false),
-        actionOnDestroy: (obj) => Destroy(obj),
+        _pool = new ObjectPool<Cube>(
+        createFunc: () => CreateCube(),
+        actionOnGet: (cube) => PrepareCube(cube),
+        actionOnRelease: (cube) => cube.gameObject.SetActive(false),
+        actionOnDestroy: (cube) => Destroy(cube),
         collectionCheck: true,
         defaultCapacity: _poolCapacity,
-        maxSize: _poolMaxSize
-            );
-    }
-
-    private void ActionOnGet(GameObject obj)
-    {
-        obj.transform.position = _startPositions[Random.Range(0, _startPositions.Length)].position;
-        obj.GetComponent<Rigidbody>().velocity = Vector3.zero;
-        obj.SetActive(true);
+        maxSize: _poolMaxSize);
     }
 
     private void Start()
     {
-        InvokeRepeating(nameof(GetSphere), 0.0f, _repeatRate);
+        InvokeRepeating(nameof(GetCube), 0.0f, _repeatRate);
     }
 
-    private void GetSphere()
+    private void ReleaseCube(Cube cube)
+    {
+        _pool.Release(cube);
+    }
+
+    private Cube CreateCube()
+    {
+        Cube cube = Instantiate(_prefabCube);
+        cube.OnReleased += ReleaseCube;
+        return cube;
+    }
+
+    private void GetCube()
     {
         _pool.Get();
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void PrepareCube(Cube cube)
     {
-        _pool.Release(other.gameObject);
+        cube.transform.position = _startPositions[Random.Range(0, _startPositions.Length)].position;
+        cube.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        cube.gameObject.SetActive(true);
     }
 }
